9743c62909571676504ac3d3a52ddf2d24d9df3b
ianstewart <ianastewart1021@gmail.com>
2024-11-10 01:26:55 -0500

    set up gui

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# rock-paper-scissors
+# Rock Paper Scissors Game
 
 ## Requirements:
 
---
+++ src/client/GameUI.java
@@ -0,0 +1,40 @@
+package client;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+public class GameUI {
+
+    private static void createGUI() throws IOException{
+        // Set up and create the JFrame window
+        JFrame frame = new JFrame("Rock Paper Scissors");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Display image
+        BufferedImage picture = ImageIO.read(new File("resources/Paper.png"));
+        JLabel picLabel = new JLabel(new ImageIcon(picture.getScaledInstance(300, 300, Image.SCALE_FAST)));
+        frame.add(picLabel);
+
+        // Display the window
+        frame.pack();
+        frame.setSize(600,500);
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) throws IOException{
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    createGUI();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
